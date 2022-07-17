cfba8ec2b94d80a101b68401b468d549d80d12e0
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-07-17 16:11:56 -0400

    created portfolio.js

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,6 +2,73 @@ import React from 'react';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
+// function NavTabs({ currentPage, handlePageChange }) {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-blue">
+//     <a className="navbar-brand" href="#">Portfolio</a>
+//     <button
+//       className="navbar-toggler"
+//       type="button"
+//       data-toggle="collapse"
+//       data-target="#navbarSupportedContent"
+//       aria-controls="navbarSupportedContent"
+//       aria-expanded="false"
+//       aria-label="Toggle navigation"
+//     >
+//       <span className="navbar-toggler-icon"></span>
+//     </button>
+
+//     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+//     <ul className="nav nav-tabs navbar-nav ml-auto">
+//       <li className="nav-item">
+//         <a
+//           href="#home"
+//           onClick={() => handlePageChange('Home')}
+//           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+//           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+//           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+//         >
+//           Home
+//         </a>
+//       </li>
+//       <li className="nav-item">
+//         <a
+//           href="#about"
+//           onClick={() => handlePageChange('About')}
+//           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+//           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+//         >
+//           About
+//         </a>
+//       </li>
+//       <li className="nav-item">
+//         <a
+//           href="#blog"
+//           onClick={() => handlePageChange('Blog')}
+//           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+//           className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+//         >
+//           Blog
+//         </a>
+//       </li>
+//       <li className="nav-item">
+//         <a
+//           href="#contact"
+//           onClick={() => handlePageChange('Contact')}
+//           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+//           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+//         >
+//           Contact
+//         </a>
+//       </li>
+//     </ul>
+//     </div>
+//     </nav>
+//   );
+// }
+
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <ul className="nav nav-tabs">
@@ -28,12 +95,12 @@ function NavTabs({ currentPage, handlePageChange }) {
       </li>
       <li className="nav-item">
         <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
-          Blog
+          Portfolio
         </a>
       </li>
       <li className="nav-item">
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <section className="containerfluid" id="applications">
+      <div className="w3-bar w3-blue">
+      </div>
+      
+      <div className="row">
+        
+        <div className="col-md-12 h3space">
+          <h3>Sample Web Applications</h3>
+        </div>
+      </div>
+          
+      <div className="row apps h3space">
+
+          <div className="col-md-4">
+            <div className="card">
+              <h5 className="card-header">Entertainment Finder<a href="https://github.com/stephencurrie/entertainment-finder" target="_blank"><img src="./assets/images/GitHub-Mark-64px.png" className="spacer" height="24px"/></a></h5>
+              <div className="card-body">
+                <a href="https://stephencurrie.github.io/entertainment-finder/index.html" target="_blank"
+            ><img src="./assets/images/entertainmentfinder2.png" alt="Entertainment Finder"/></a> 
+              </div>
+              <p className="small">(Click image to go to application)</p>
+              
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="card">
+              <h5 className="card-header">Weather Dashboard<a href="https://github.com/stephencurrie/hw-6-weather-dashboard" target="_blank"><img src="./assets/images/GitHub-Mark-64px.png" className="spacer" height="24px"/></a></h5>
+              <div className="card-body">
+                <a href="https://stephencurrie.github.io/hw-6-weather-dashboard/" target="_blank"
+            ><img src="./assets/images/weather_screen2.png" alt="Weather Dashboard"/></a> 
+              </div>
+              <p className="small">(Click image to go to application)</p>
+              
+            </div>
+          </div>
+
+
+        <div className="col-md-4">
+          <div className="card">
+            <h5 className="card-header">The Prioritizer<a href="https://github.com/mdschenck/Prioritizer" target="_blank"><img src="./assets/images/GitHub-Mark-64px.png" className="spacer" height="24px"/></a></h5>
+            <div className="card-body">
+              <a href="https://infinite-coast-94356.herokuapp.com/" target="_blank"
+          ><img src="./assets/images/prioritizer_screen2.png" alt="Prioritizer"/></a> 
+            </div>
+            <p className="small">(Click image to go to application)</p>
+            
+          </div>
+        </div>
+
+      </div>
+
+
+    </section>
+    </div>
+  );
+}
