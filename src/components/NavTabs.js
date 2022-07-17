dbf7782e0bf6bd8f4320c8a1df0bae56ef612cae
scurrie <99137578+stephencurrie@users.noreply.github.com>
2022-07-17 16:50:41 -0400

    updated resume.js

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -74,23 +74,13 @@ function NavTabs({ currentPage, handlePageChange }) {
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
           href="#about"
           onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          // This is a conditional (ternary) operator that checks to see if the current page is "About Me"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
         >
-          About
+          About Me
         </a>
       </li>
       <li className="nav-item">
@@ -107,10 +97,20 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#contact"
           onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          // Check to see if the currentPage is `Contact Me`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
-          Contact
+          Contact Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
         </a>
       </li>
     </ul>
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <section className="containerfluid" id="skills">
+      <div className="row">
+
+    <div className="col-md-8 h3space"> 
+      <h3>Business Skills</h3>
+      <ul>
+        <li>
+          <strong>Technology Leader</strong> Ability to create high
+          performing teams, Establishes clarity in complex environments,
+          Growth oriented, Understands how to use technology to achieve
+          business results, Optimizes costs. Builds cross-functional
+          partnerships, Develops practical strategies and executes
+        </li>
+
+        <li>
+          <strong>Drive for Results:</strong> Goes beyond job
+          responsibilities to achieve success
+        </li>
+        <li>
+          <strong>Business Acumen:</strong> Objectively looks at data to
+          determine and execute optimal plan
+        </li>
+        <li>
+          <strong>Collaboration and Partnership:</strong> Ability to work
+          with a range of personalities, focus on teamwork, culture,
+          process and end-results
+        </li>
+        <li>
+          <strong>Drive for Results:</strong> Goes beyond job
+          responsibilities to achieve success
+        </li>
+        <li>
+          <strong>Functional Knowledge:</strong> Application Development,
+         Hosting, Networking, Security, Workforce
+          Productivity, Global Operations, IT Service Management,
+          Experience in both Consumer and Business space
+        </li>
+      </ul>
+    </div>
+
+
+    <div className="col-md-4 h3space">
+      <h3>Technical Skills</h3>
+      <ul className="techskills">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>Object Oriented Programming</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL</li>
+        <li>Mongoose NoSQL</li>
+        <li>Sequelize</li>
+        <li>Object Relational Mapping (ORM)</li>
+        <li>Model View Controller (MVC)</li>
+        <li>Responsive Design</li>
+      </ul>
+    </div>
+    </div>
+
+</section>
+</div>
+
+  );
+}
